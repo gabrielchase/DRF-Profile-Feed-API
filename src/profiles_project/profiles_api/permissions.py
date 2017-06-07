0b46ef99d6f451b804ebda912b7a79949806a080
gabrielchase <gchase.patron@gmail.com>
2017-06-07 16:14:25 +0800

    Permission to change own status

--- src/profiles_project/profiles_api/permissions.py
+++ src/profiles_project/profiles_api/permissions.py
@@ -11,3 +11,16 @@ class UpdateOwnProfile(permissions.BasePermission):
             return True
 
         return obj.id == request.user.id
+
+class PostOwnStatus(permissions.BasePermission):
+    """Allow users to edit their own status."""
+
+    def has_object_permission(self, request, view, obj):
+        """Check user is trying to edit their own status."""
+
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
+        print('obj: {0}\nview: {1}'.format(obj, view))
+
+        return obj.user_profile.id == request.user.id
