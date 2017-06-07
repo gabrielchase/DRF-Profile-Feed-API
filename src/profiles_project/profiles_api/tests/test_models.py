d37906b5d5fdd857cc50dcdecc995c8505de3724
gabrielchase <gchase.patron@gmail.com>
2017-06-07 20:28:38 +0800

    Separated model tests

--- src/profiles_project/profiles_api/tests/test_models.py
+++ src/profiles_project/profiles_api/tests/test_models.py
@@ -7,12 +7,27 @@ class TestUserProfile:
         obj = mixer.blend('profiles_api.UserProfile')
 
         assert obj.pk == 1, 'Should create a User Profile instance'
+    
+    def test_model_full_name(self):
+        obj = mixer.blend('profiles_api.UserProfile')
+
         assert obj.name == obj.get_full_name(), 'Object name should be returned'
+        
+    def test_model_short_name(self):
+        obj = mixer.blend('profiles_api.UserProfile')
         assert obj.name == obj.get_short_name(), 'Object name should be returned'
+
+    def test_model__str__(self):
+        obj = mixer.blend('profiles_api.UserProfile')
         assert obj.email == str(obj), "Object's string representation is its email"
 
 class TestProfileFeedItem:
     def test_model(self):
         obj = mixer.blend('profiles_api.ProfileFeedItem')
 
-        assert obj.status_text == str(obj), "Object's string representation is the statu_text"
+        assert obj.pk == 1, 'Should create a Profile Feed Item instance'
+
+    def test_model__str__(self):
+        obj = mixer.blend('profiles_api.ProfileFeedItem')
+
+        assert obj.status_text == str(obj), "Object's string representation is the status_text"
